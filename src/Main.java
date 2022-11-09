799335e68f52b421f988f6562da40f2689085230
Alexandr Yakovlev <engelsovich75@yandex.ru>
2022-11-09 16:56:26 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<Person> queue = new LinkedList<>(generateClients());
+
+        while (true) {
+            if (!queue.isEmpty()) {
+                Person person = queue.poll();
+                System.out.println(person.name + " " + person.surname + " прокатился(лась) на аттракционе");
+                person.numOfTickets--;
+                if (!(person.numOfTickets == 0)) {
+                    queue.add(person);
+                }
+            } else {
+                break;
+            }
+        }
+    }
+
+    private static List<Person> generateClients() {
+        return List.of(
+            new Person("Дмитрий", "Яковлев", 5),
+            new Person("Алексей", "Хасанов", 4),
+            new Person("Арсений", "Яковлев", 3),
+            new Person("Анастасия", "Хасанова", 2),
+            new Person("Иван", "Яковлев", 1)
+        );
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    String name;
+    String surname;
+    int numOfTickets;
+
+    public Person (String name, String surname, int numOfTickets) {
+        this.name = name;
+        this.surname = surname;
+        this.numOfTickets = numOfTickets;
+    }
+}
